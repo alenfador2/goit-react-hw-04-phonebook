28b60edbdc0d2e78295c863e8b4840c39c33be54
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-02-13 17:06:55 +0100

    Fix update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
